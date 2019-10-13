012856ca9c4b81fa3d9e382d7fa7108e65b1aba5
dscoursera2019 <ahosny.eg@gmail.com>
2019-10-13 22:01:40 +0300

    Commit R Files + PNG

--- plot1.R
+++ plot1.R
@@ -1,1 +1,9 @@
 #Plot 1
+#Read Data
+power <- read.table("epc/household_power_consumption.txt",skip=1,sep=";")
+#Name Columns
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+#Subset Data
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+#Call Hist
+hist(as.numeric(as.character(subpower$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
